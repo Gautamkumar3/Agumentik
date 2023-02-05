8f17a1b9bfd0cacea3bf70be6bef5bc38565a3e4
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-02-05 22:13:28 +0530

    resources section done

--- agumentic-assignment/src/Component/Resources.jsx
+++ agumentic-assignment/src/Component/Resources.jsx
@@ -1,12 +1,22 @@
-import { Box, Center, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import { FaCreativeCommonsNd } from "react-icons/fa";
+import logo1 from "../assets/logo1.png";
+import logo2 from "../assets/logo2.png";
 
 const Resources = () => {
   return (
     <Box my={"5%"}>
       <Center>
-        <Box>
+        <Box mb={10}>
           <Center>
             <Box
               borderRadius={"50%"}
@@ -20,26 +30,105 @@ const Resources = () => {
             </Box>
           </Center>
 
-          <Heading my={3} color={"#fff"} mb={8}>
+          <Heading my={3} color={"#fff"}>
             Resources
           </Heading>
         </Box>
       </Center>
-      <SimpleGrid columns={2} spacing={10}>
-        <Box>
-          <Text>ASSET SHOP FEATURING</Text>
-          <SimpleGrid columns={2} spacing={5}></SimpleGrid>
+      <SimpleGrid columns={2} spacing={10} w="80%" margin={"auto"}>
+        <Box color={"#fff"}>
+          <Text fontSize={"20px"}>
+            <b>ASSET SHOP FEATURING</b>{" "}
+          </Text>
+          <SimpleGrid columns={2} spacing={5} py={5}>
+            <Box
+              textAlign={"center"}
+              boxShadow="md"
+              p="6"
+              rounded="md"
+              bg="#333"
+            >
+              <Center bg="#333">
+                {" "}
+                <Image bg="#333" w={"25px"} src={logo1} />
+              </Center>
+
+              <Text fontSize={"12px"} bg="#333" my={2} color="gray.300">
+                Lorem ipsum dolor sit.
+              </Text>
+              <Text fontWeight={"bold"} bg="#333" my={1}>
+                100+
+              </Text>
+            </Box>
+            <Box
+              textAlign={"center"}
+              boxShadow="md"
+              p="6"
+              rounded="md"
+              bg="#333"
+            >
+              <Center bg="#333">
+                {" "}
+                <Image bg="#333" w={"25px"} src={logo2} />
+              </Center>
+
+              <Text fontSize={"12px"} bg="#333" my={2} color="gray.300">
+                Lorem ipsum .
+              </Text>
+              <Text fontWeight={"bold"} bg="#333" my={1}>
+                12
+              </Text>
+            </Box>
+            <Box
+              textAlign={"center"}
+              boxShadow="md"
+              p="6"
+              rounded="md"
+              bg="#333"
+            >
+              <Center bg="#333">
+                {" "}
+                <Image bg="#333" w={"25px"} src={logo2} />
+              </Center>
+
+              <Text fontSize={"12px"} bg="#333" my={2} color="gray.300">
+                ipsum dolo.
+              </Text>
+              <Text fontWeight={"bold"} bg="#333" my={1}>
+                3
+              </Text>
+            </Box>
+            <Box
+              textAlign={"center"}
+              boxShadow="md"
+              p="6"
+              rounded="md"
+              bg="#333"
+            >
+              <Center bg="#333">
+                {" "}
+                <Image bg="#333" w={"25px"} src={logo1} />
+              </Center>
+
+              <Text fontSize={"12px"} bg="#333" my={2} color="gray.300">
+                Lorem dolor sit.
+              </Text>
+              <Text fontWeight={"bold"} bg="#333" my={1}>
+                2500+
+              </Text>
+            </Box>
+          </SimpleGrid>
         </Box>
 
-        <Box>
+        <Flex justify={"center"} align="center">
           <Image
-            maxH={"250px"}
-            maxW="250px"
+            maxH={"300px"}
+            maxW="300px"
             borderRadius={"20px"}
             border="4px solid #fff"
             src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZnVybml0dXJlJTIwaW1hZ2UlMjBpbnNpZGUlMjBoZXhhZ29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
           />
-        </Box>
+        </Flex>
       </SimpleGrid>
     </Box>
   );
